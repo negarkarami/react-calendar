2a5f9bbbde3e79fa1bdcb2fa59647c6f80b5100d
negarkarami <inegarkarami@gmail.com>
2022-04-28 20:28:12 +0430

    first commit react-calendar

---
+++ src/DeleteEventModal/DeleteEventModal.js
@@ -0,0 +1,18 @@
+
+
+export const DeleteEventModal = ({ onDelete, eventText, onClose }) => {
+    return(
+        <>
+            <div id="deleteEventModal">
+                <h2>Event</h2>
+
+                <p id="eventText">{eventText}</p>
+
+                <button onClick={onDelete} id="deleteButton">Delete</button>
+                <button onClick={onClose} id="closeButton">Close</button>
+            </div>
+
+            <div id="modalBackDrop"></div>
+        </>
+    );
+};
